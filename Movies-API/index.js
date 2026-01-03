4443b17657746a3a9e09e99abb620c90261fd3ec
zaid smadi <smadizaid15@gmail.com>
2026-01-03 20:42:36 +0300

    Add movies API with POST and GET endpoints
    Implement basic CRUD operations for movies API.

--- Movies-API/index.js
+++ Movies-API/index.js
@@ -1,1 +1,39 @@
+const express=require('express');
+const app=express();
+const port=5500;
+app.use(express.json());
+let movies=[];
+
+
+app.listen(
+  port,
+  ()=>{console.log(`live on http://localhost:${port}`);
+});
+
+
+app.post("/movies",(req,res=>{
+  const {title,year}=req.body;
+    const movie={
+      id:movies.length+1,
+      title,
+      year
+    };
+     movies.push(movie);
+     res.status(201).json(movie);
+});
+
+
+app.get("/movies",(req,res)=>{
+  res.json(movies);
+});
+
+
+app.get("/movies/:id",(req,res)=>{
+    const id=Number(req.params.id);
+    const movie=movies.find(m=>m.id===id);
+     if(!movie) {
+       return res.status(404).json({message:"movie not found"});
+     }
+}):
+
 
