53410818493e3928ac601feb7a6a214c233bfd12
zaid smadi <smadizaid15@gmail.com>
2026-01-03 20:32:07 +0300

    Implement basic CRUD operations for products API

--- products-API/index.js
+++ products-API/index.js
@@ -1,1 +1,41 @@
+const express=require(express);
+const app=express():
+const port=4500;
+app.use(express.json());
+let products=[];
 
+
+app.listen(
+  port,
+  ()=>{console.log(`listening on http://localhost:${port}`);
+      });
+
+
+app.post("/products",(req,res)=>{
+  const {name,price}=req.body;
+    const product={
+      id:products.length+1,
+      name,
+      price
+    };
+    products.push(product);
+    res.status(201).json(product);
+});
+
+
+app.get(/"products",(req,res)=>{
+    res.json(products);
+});
+
+
+app.delete("/products/:id",(req,res)=>{
+  const id=Number(req.params.id);
+  products=products.filter(p=>p.id!==id);
+  res.json({message:"Product deleted"});
+});
+
+  
+  
+
+
+  
